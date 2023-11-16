8f27acfe582a4d03e00d4e551569eb8161a1d957
Vitaliy Didyk <79630556+MrTartuf0@users.noreply.github.com>
2023-11-16 07:18:23 -0800

    Update app.config.ts

--- app.config.ts
+++ app.config.ts
@@ -1,24 +1,7 @@
 export default defineAppConfig({
   docus: {
-    title: 'Docus',
-    description: 'The best place to start your documentation.',
-    image: 'https://user-images.githubusercontent.com/904724/185365452-87b7ca7b-6030-4813-a2db-5e65c785bf88.png',
-    socials: {
-      twitter: 'nuxt_js',
-      github: 'nuxt-themes/docus',
-      nuxt: {
-        label: 'Nuxt',
-        icon: 'simple-icons:nuxtdotjs',
-        href: 'https://nuxt.com'
-      }
-    },
-    github: {
-      dir: '.starters/default/content',
-      branch: 'main',
-      repo: 'docus',
-      owner: 'nuxt-themes',
-      edit: true
-    },
+    title: 'Application Layer',
+    description: 'Relazione dettagliata sul livello applicativo dell\'iso/osi.',
     aside: {
       level: 0,
       collapsed: false,
